78cccfe1b0c81ebff41a0db5ff81a4e131dd4b16
Alexander Ulianovskiy <131038990+ognerub@users.noreply.github.com>
2025-09-03 09:25:39 +0300

    Add files via upload

---
+++ EffectiveMobile/EffectiveMobileApp.swift
@@ -0,0 +1,20 @@
+//
+//  EffectiveMobileApp.swift
+//  EffectiveMobile
+//
+//  Created by Alexander Ognerubov on 03.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct EffectiveMobileApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
